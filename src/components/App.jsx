2dd9085168e87e981b49db4981d6e149e8518fa1
Rafael Nobre Queiroz <rafaelnobrequeiroz@gmail.com>
2022-03-16 16:09:42 -0300

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import "../styles/App.css"
+import Pokedex from './Pokedex';
+import pokemons from '../pokedex.json';
+
+
+export default class App extends Component {
+
+  state = {
+    pokemons,
+    pokemon: pokemons[0]
+  }
+
+  update = (pokemon) => {
+    this.setState({ pokemon });
+  }
+
+  getPokemon = () => {
+    const { id } = this.state.pokemon;
+    console.log(id.lenght)
+    if (id.toString().length === 1) return `00${id}`
+    if (id.toString().length === 2) return `0${id}`
+    if (id.toString().length === 3) return `${id}`
+  }
+
+  getTypes = () => {
+    let phrase;
+    const { type } = this.state.pokemon;
+    if (type.length === 1) return type[0]
+    if (type.length > 1) {
+      for (let index = 0; index < type.length; index++) {
+        if (index === 0) phrase = type[index]
+        if (index !== 0) phrase = `${phrase} and ${type[index]}`
+      }
+      return phrase
+    }
+  }
+
+  render() {
+    const { pokemons } = this.state; 
+    const { name } = this.state.pokemon;
+    return (
+      <div style={{ display: "flex" }}>
+        <div className="container">
+          <img id="pokemonImg" src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${this.getPokemon()}.png`} alt={name} />
+          <p id="name">{`${name} is a pokemon type ${this.getTypes()}`}</p>
+        </div>
+        <Pokedex updateParent={this.update} pokemons={pokemons} />
+      </div>
+    )
+  }
+}
